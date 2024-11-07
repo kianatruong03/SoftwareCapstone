1418383d51e527ca09d13acf37ac419b817d046a
Jenna Constantino <97635991+JennaConstantino@users.noreply.github.com>
2024-11-07 14:53:36 -0500

    changes

--- queencitysocial/src/App.js
+++ queencitysocial/src/App.js
@@ -6,23 +6,42 @@ import HomeEventFeed from './pages/HomeEventFeed'
 import SpotlightEvent from './pages/SpotlightEvent'
 import AccountManagement from './pages/AccountManagement'
 import EventPage from './pages/EventPage'
-import LoginSignUp from './pages/LoginSignUp'
+import Login from './pages/Login'
+import Signup from './pages/signup';
 
-function App() {
-  return ( // Routing to different website endpoints
-    <div>
+const App = () => {
+  return (
+    <div className="App">
       <BrowserRouter>
         <Routes> 
           <Route index element = {<HomeEventFeed />}/>
           <Route path = "/home" element = {<HomeEventFeed />}/>
           <Route path = "/spotlight" element = {<SpotlightEvent />}/>
           <Route path = "/event" element = {<EventPage />}/> {/* Edit event pathing as it will be decided via api call to db, event#{event_id} type? */}
-          <Route path = "/login" element = {<LoginSignUp />}/>
+          <Route path = "/login" element = {<Login />}/>
+          <Route path = "/signup" element = {<Signup />}/>
           <Route path = "/account" element = {<AccountManagement />}/>
+          
         </Routes>
-      </BrowserRouter>
+    </BrowserRouter>
+
     </div>
   );
-}
+};
 
 export default App;
+
+<div>
+<BrowserRouter>
+  <Routes> 
+    <Route index element = {<HomeEventFeed />}/>
+    <Route path = "/home" element = {<HomeEventFeed />}/>
+    <Route path = "/spotlight" element = {<SpotlightEvent />}/>
+    <Route path = "/event" element = {<EventPage />}/> {/* Edit event pathing as it will be decided via api call to db, event#{event_id} type? */}
+    <Route path = "/login" element = {<Login />}/>
+    <Route path = "/signup" element = {<Signup />}/>
+    <Route path = "/account" element = {<AccountManagement />}/>
+    
+  </Routes>
+</BrowserRouter>
+</div>
