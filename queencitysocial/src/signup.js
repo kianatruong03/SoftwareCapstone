8e4005164f2ab669f8be3079b3f70691273a6ef0
Kiana Truong <kianatruong03@gmail.com>
2024-11-07 19:33:54 -0500

    finally fixed the css issue with the user and account, will now work on the edit/post for the user save information to the database.

--- queencitysocial/src/signup.js
+++ queencitysocial/src/signup.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './login_signup.css';
+import './Collective.css';
 import Logo from './Photos/CapstoneLogo.png';
 
 const Signup = ({ setIsLogin }) => {
@@ -32,32 +32,34 @@ const Signup = ({ setIsLogin }) => {
   };
 
   return (
-    <div className="container">
-      <div className="illustration-section">
-        <div className="illustration">
-          <img src="https://raw.githubusercontent.com/kianatruong03/SoftwareCapstone/refs/heads/tariq-dev/queencitysocial/public/undraw_Newspaper_re_syf5-removebg-preview.png" alt="Signup Illustration" />
+    <div className="login-signup-wrapper">
+      <div className="user-container">
+        <div className="illustration-section">
+          <div className="illustration">
+            <img src="https://raw.githubusercontent.com/kianatruong03/SoftwareCapstone/refs/heads/tariq-dev/queencitysocial/public/undraw_Newspaper_re_syf5-removebg-preview.png" alt="Signup Illustration" />
+          </div>
         </div>
-      </div>
-      <div className="form-section">
-      <img className="Logo" src={Logo} alt="Profile Avatar" />
-        <h2>Create Account</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <input type="text" name="firstName" placeholder="First Name" required />
+        <div className="user-form-section">
+        <img className="Logo" src={Logo} alt="Profile Avatar" />
+          <h2>Create Account</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="user-form-group">
+              <input type="text" name="firstName" placeholder="First Name" required />
+            </div>
+            <div className="user-form-group">
+              <input type="text" name="lastName" placeholder="Last Name" required />
+            </div>
+            <div className="user-form-group">
+              <input type="email" name="email" placeholder="Email Address" required />
+            </div>
+            <div className="user-form-group">
+              <input type="password" name="password" placeholder="Password" required />
+            </div>
+            <button type="submit" className="signup-button">Sign Up</button>
+          </form>
+          <div className="switch-form">
+            Already have an account? <a onClick={() => setIsLogin(true)}>Login</a>
           </div>
-          <div className="form-group">
-            <input type="text" name="lastName" placeholder="Last Name" required />
-          </div>
-          <div className="form-group">
-            <input type="email" name="email" placeholder="Email Address" required />
-          </div>
-          <div className="form-group">
-            <input type="password" name="password" placeholder="Password" required />
-          </div>
-          <button type="submit" className="signup-button">Sign Up</button>
-        </form>
-        <div className="switch-form">
-          Already have an account? <a onClick={() => setIsLogin(true)}>Login</a>
         </div>
       </div>
     </div>
